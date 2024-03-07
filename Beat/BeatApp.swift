6f5bd388888001c59f79e0a55a61916b07efcaf7
Víctor Barrios <sygmaz19@gmail.com>
2024-03-07 18:59:35 +0100

    Initial Commit

---
+++ Beat/BeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatApp.swift
+//  Beat
+//
+//  Created by Víctor Barrios Sánchez on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
