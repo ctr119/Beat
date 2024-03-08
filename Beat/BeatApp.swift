feed63798835f5ed9a257d1202792b4eb2343d41
Victor Barrios <victor.barrios@bestsecret.com>
2024-03-08 22:05:36 +0100

    [refactor] Adapt App and Content views

--- Beat/BeatApp.swift
+++ Beat/BeatApp.swift
@@ -4,8 +4,7 @@ import SwiftUI
 struct BeatApp: App {
     var body: some Scene {
         WindowGroup {
-//            ContentView()
-            TopAlbumsViewFactory.make()
+            ContentView()
         }
     }
 }
