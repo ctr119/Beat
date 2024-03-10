42b85f6d5b91ed336c36e11df41ab62a63aaefde
Victor Barrios <victor.barrios@bestsecret.com>
2024-03-10 09:09:31 +0100

    [feat] Add search bar

--- Beat/Clean/Presentation/Search/SearchView.swift
+++ Beat/Clean/Presentation/Search/SearchView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchQuery = ""
+    
     @State private var viewModel: SearchViewModel
     @State private var router: AlbumsListRouter
     
@@ -11,22 +13,38 @@ struct SearchView: View {
     
     var body: some View {
         NavigationStack(path: $router.navigationPath) {
-            AlbumsListView(
-                albums: viewModel.albums,
-                rowBuilder: { album in
-                    AlbumListGalleryRow(album: album)
-                },
-                onItemTapped: { album in
-                    router
-                        .navigationPath
-                        .navigate(to: .albumDetails(id: album.id))
-                }
-            )
+            VStack {
+                AlbumsListView(
+                    albums: viewModel.albums,
+                    rowBuilder: { album in
+                        AlbumListGalleryRow(album: album)
+                    },
+                    onItemTapped: { album in
+                        router
+                            .navigationPath
+                            .navigate(to: .albumDetails(id: album.id))
+                    }
+                )
+                
+                TextField("Search...", text: $searchQuery)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.gray.opacity(0.3))
+                    )
+                    .padding()
+                    .onSubmit {
+                        Task {
+                            await viewModel.performSearch(query: searchQuery)
+                        }
+                    }
+            }
             .navigationTitle("Explore...")
             .navigationDestination(for: AlbumsListRouter.Destination.self) { destination in
                 router.view(for: destination)
             }
         }
+        .tint(Color.yellow)
     }
 }
 
